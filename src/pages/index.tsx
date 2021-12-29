fdfbfc4f50d8c1bf4958ed17517e167209648c8a
Adrien Wald <adrien@genei.io>
2021-12-29 02:09:04 +0100

    slap jeff

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,37 @@
 import type { NextPage } from 'next'
+import Image from 'next/image'
 import Box from '@mui/material/Box'
 import Cursor from '@/components/Cursor'
 
 const Home: NextPage = () => {
   return (
     <Box sx={{ width: '100%', height: '100vh' }}>
-      <Cursor>
+      <Cursor
+        renderCursor={() => (
+          <Image
+            src='/slap.png'
+            alt='slap hand'
+            width={194}
+            height={316}
+            layout='fixed'
+          />
+        )}
+      >
         <Box
           sx={{
             width: '100%',
             height: '100%',
-            backgroundColor: (theme) => (theme.palette.secondary.main)
+            display: 'flex',
+            justifyContent: 'center'
           }}
         >
+          <Image
+            src='/jeff.png'
+            alt='jeff'
+            width={1093}
+            height={873}
+            layout='fixed'
+          />
         </Box>
       </Cursor>
     </Box>
